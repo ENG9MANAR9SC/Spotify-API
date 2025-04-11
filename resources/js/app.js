ae058a09d982efb8b13af2311ea77d633a3f0f1e
MANAR <122207957+ENG9MANAR9SC@users.noreply.github.com>
2025-04-11 11:05:02 +0200

    install vue js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 import './bootstrap';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
+import login from "../views/components/login.vue";
+
+
+
+const app = createApp({
+    components: {
+      'login': login // Register the component locally
+    }
+  });
+
+
+app.mount('#app');
